357e71c27ade63ff019231c133c2a1292bbd5e4d
Saša Barišić <sasa.barisic@serengetitech.com>
2022-11-03 16:11:49 +0100

    Start of injector calculator

---
+++ InjectorCalculator/InjectorCalculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Numerics;
+using Raylib_cs;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using static Raylib_cs.Raylib;
+
+namespace InjectorCalculator {
+	class Program {
+		const int W = 800;
+		const int H = 600;
+		static Vector2 Origin;
+		static Font TxtFnt;
+
+		static RaylibGrid Grid;
+
+		static void Main(string[] args) {
+			SetConfigFlags(ConfigFlags.FLAG_MSAA_4X_HINT);
+			SetConfigFlags(ConfigFlags.FLAG_WINDOW_HIGHDPI);
+			InitWindow(W, H, "Injector Calculator");
+			SetTargetFPS(30);
+
+			Origin = new Vector2(50, 50);
+			TxtFnt = LoadFont("consola.ttf");
+
+			Grid = new RaylibGrid(new Vector2(20, 20), new Vector2(W - 100, H - 100), "PWidth [ms]", "Flow");
+
+			LoadData();
+
+			while (!WindowShouldClose())    // Detect window close button or ESC key
+			{
+				BeginDrawing();
+				ClearBackground(Color.BLACK);
+				Grid.Draw();
+				EndDrawing();
+
+				Vector2 MousePos = GetMousePosition();
+				MousePos = new Vector2(MousePos.X, H - MousePos.Y);
+
+
+				//bool Left = IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT);
+				//bool Right = IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_RIGHT);
+			}
+
+			CloseWindow();
+		}
+
+		static void LoadData() {
+		}
+	}
+}
---
+++ InjectorCalculator/RaylibGrid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+using System.Numerics;
+using static Raylib_cs.Raylib;
+
+namespace InjectorCalculator {
+	class RaylibGrid {
+		public Vector2 Pos;
+		public Vector2 Size;
+
+		public string XName;
+		public string YName;
+
+		public RaylibGrid(Vector2 Pos, Vector2 Size, string XName, string YName) {
+			this.Pos = Pos;
+			this.Size = Size;
+			this.XName = XName;
+			this.YName = YName;
+		}
+
+
+		public void Draw() {
+
+		}
+
+	}
+}
